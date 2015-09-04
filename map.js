30616a53c3e1603f9a74bed3143ec9758440bcd6
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-09-03 17:21:59 -0700

    The code is in separated files

---
+++ map.js
@@ -0,0 +1,20 @@
+var latitude;
+var longitude;
+    getLocation();
+  function getLocation() {
+     if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(showLocation);
+    }
+  }
+  function showLocation(position) {
+     latitude=position.coords.latitude;
+     longitude=position.coords.longitude;
+   } 
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: latitude, lng: longitude},
+    zoom: 15
+  });
+}
