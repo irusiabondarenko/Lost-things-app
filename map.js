16b69401a1867cfaf04ad39962ce4229c5e87366
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-09-13 12:48:33 -0700

    Now map is showing

--- map.js
+++ map.js
@@ -1,15 +1,16 @@
 var latitude;
 var longitude;
-    getLocation();
-  function getLocation() {
-     if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(showLocation);
-    }
-  }
-  function showLocation(position) {
-     latitude=position.coords.latitude;
-     longitude=position.coords.longitude;
-   } 
+getLocation();
+function getLocation() {
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(showLocation);
+	}
+}
+function showLocation(position) {
+	latitude=position.coords.latitude;
+	longitude=position.coords.longitude;
+	initMap();
+} 
 
 var map;
 function initMap() {
