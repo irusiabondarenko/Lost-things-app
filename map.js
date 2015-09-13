a65e94e0789a72f1f29942262bbfcc0450d1d372
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-09-13 15:12:38 -0700

    Server is serving data

--- map.js
+++ map.js
@@ -1,37 +1,3 @@
-
-var LostThing = function() {
-	this.coord = {
-		lng:'',
-		lat:''
-	};
-	this.title = '';
-	this.description = '';
-	this.contact = '';
-	this.photoURL = '';
-}
-
-/*LostThing.prototype = {
-};*/
-
-var testData = [];
-var lostThing = new LostThing();
-lostThing.coord.lat = 47.593367;
-lostThing.coord.lng = -122.125279;
-lostThing.title = 'Ball';
-lostThing.description = 'It is smiling,';
-lostThing.photoURL = 'http://www.phoenixparkbook.com/funnyball.gif';
-lostThing.contact = 'a@gmail.com';
-testData.push(lostThing);
-
-lostThing = new LostThing();
-lostThing.coord.lat = 47.608762;
-lostThing.coord.lng = -122.097298;
-lostThing.title = 'Dog';
-lostThing.description = 'I was swimming on kayak with my dog. And it dissapeared...';
-lostThing.photoURL = 'http://www.splotchy.com/images/blog/puppy.jpg';
-lostThing.contact = 'b@gmail.com';
-testData.push(lostThing);
-
 // TODO: Use AngularJS
 function getLostThingContent(lostThing){
 	var titleDiv = '<div>' + lostThing.title + '</div>';
@@ -67,7 +33,14 @@ function getLocation() {
 }
 function showLocation(position) {
 	var map = initMap({lat: position.coords.latitude, lng: position.coords.longitude});
-	visualizeLostThings(testData, map);
+	// Use real host instead of localhost 
+	$.ajax({
+		url: 'http://localhost:8080/getdata',
+		success: function(data) {
+			var testData = data;
+			visualizeLostThings(testData, map);
+		}
+	});
 } 
 
 function initMap(coord) {
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var app = express();
 
 app.use(express.static(__dirname));
 
+var testData = [];
+
 app.get('/', function (req, res) {
   res.sendFile('map.html', {root: __dirname});
 });
 
-app.get('/Test', function(req, res){
-	res.send('Bla');
+app.get('/getdata', function(req, res){
+	res.send(testData);
 });
 
 var server = app.listen(8080, function(){
@@ -17,3 +19,33 @@ var server = app.listen(8080, function(){
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+var LostThing = function() {
+	this.coord = {
+		lng:'',
+		lat:''
+	};
+	this.title = '';
+	this.description = '';
+	this.contact = '';
+	this.photoURL = '';
+}
+
+var lostThing = new LostThing();
+lostThing.coord.lat = 47.593367;
+lostThing.coord.lng = -122.125279;
+lostThing.title = 'Ball';
+lostThing.description = 'It is smiling,';
+lostThing.photoURL = 'http://www.phoenixparkbook.com/funnyball.gif';
+lostThing.contact = 'a@gmail.com';
+testData.push(lostThing);
+
+lostThing = new LostThing();
+lostThing.coord.lat = 47.608762;
+lostThing.coord.lng = -122.097298;
+lostThing.title = 'Dog';
+lostThing.description = 'I was swimming on kayak with my dog. And it dissapeared...';
+lostThing.photoURL = 'http://www.splotchy.com/images/blog/puppy.jpg';
+lostThing.contact = 'b@gmail.com';
+testData.push(lostThing);
