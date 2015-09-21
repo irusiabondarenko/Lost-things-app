e5a8706d93e1023e26ad689c63a2d00426225c8f
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-09-20 18:34:46 -0700

    Added button for creating new spot

---
+++ map.js
@@ -0,0 +1,96 @@
+// POST example
+// $.ajax({url: 'http://localhost:8080/add', type: 'POST', data: JSON.stringify({a: 'str', b: 123}), contentType: "application/json; charset=utf-8"})
+
+// TODO: Use AngularJS
+
+
+function getLostThingContent(lostThing){
+	var titleDiv = '<div>' + lostThing.title + '</div>';
+	var descriptionDiv = '<div>' + lostThing.description + '</div>';
+	var photoImg = '<img src="' + lostThing.photoURL + '"></img>';
+	var contactDiv = '<div>' +  lostThing.contact +'</div>';
+	return titleDiv + descriptionDiv + photoImg + contactDiv;
+};
+var map_var;
+function visualizeLostThings(lostThings, map){
+	lostThings.forEach(function(item){
+		
+		var marker = new google.maps.Marker({
+			position: item.coord,
+			map: map,
+			title: item.title,
+			animation: google.maps.Animation.DROP,
+		});
+		var infowindow = new google.maps.InfoWindow({
+			content: getLostThingContent(item)
+		});
+		marker.addListener('click', function(){
+			infowindow.open(map, marker);
+		});
+		
+	});
+}
+
+
+function getLocation() {
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(showLocation);
+	}
+}
+var currentPosition;
+function showLocation(position) {
+	currentPosition = position;
+	var map = initMap({lat: position.coords.latitude, lng: position.coords.longitude});
+	// Use real host instead of localhost 
+	$.ajax({
+		url: 'http://localhost:8080/getdata',
+		success: function(data) {
+			var testData = data;
+			visualizeLostThings(testData, map);
+		}
+	});
+	
+} 
+
+function initMap(coord) {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: coord,
+    zoom: 12
+  });
+  map_var = map;
+  return map;
+}
+
+$(function(){
+	getLocation();	
+});
+
+var LostThing = function() {
+	this.coord = {
+		lng:'',
+		lat:''
+	};
+	this.title = '';
+	this.description = '';
+	this.contact = '';
+	this.photoURL = '';
+}
+
+
+function message(){
+	var lostThing = new LostThing();
+    lostThing.coord = {lat: currentPosition.coords.latitude, lng: currentPosition.coords.longitude};
+	lostThing.title = prompt("Enter title");
+	//testData.push(lostThing);
+    $.ajax({
+		url: 'http://localhost:8080/add', 
+		type: 'POST', 
+		data: JSON.stringify(lostThing), 
+		contentType: "application/json; charset=utf-8",
+		success: function(testData){
+			visualizeLostThings(testData, map_var);		
+		}
+	})
+
+}
+
---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+var testData = [];
+
+app.get('/', function (req, res) {
+  res.sendFile('map.html', {root: __dirname});
+});
+
+app.get('/getdata', function(req, res){
+	res.send(testData);
+});
+
+app.post('/add', function(req, res){
+	testData.push(req.body);
+    res.send(testData);
+});
+
+var server = app.listen(8080, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+var LostThing = function() {
+	this.coord = {
+		lng:'',
+		lat:''
+	};
+	this.title = '';
+	this.description = '';
+	this.contact = '';
+	this.photoURL = '';
+}
+
+
+var lostThing = new LostThing();
+lostThing.coord.lat = 47.593367;
+lostThing.coord.lng = -122.125279;
+lostThing.title = 'Ball';
+lostThing.description = 'It is smiling,';
+lostThing.photoURL = 'http://www.phoenixparkbook.com/funnyball.gif';
+lostThing.contact = 'a@gmail.com';
+testData.push(lostThing);
+
+lostThing = new LostThing();
+lostThing.coord.lat = 47.608762;
+lostThing.coord.lng = -122.097298;
+lostThing.title = 'Dog';
+lostThing.description = 'I was swimming on kayak with my dog. And it dissapeared...';
+lostThing.photoURL = 'http://www.splotchy.com/images/blog/puppy.jpg';
+lostThing.contact = 'b@gmail.com';
+testData.push(lostThing);
+
+
