3abbe665a6710d58197b7b1ad7f8b7d3bff21102
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-10-02 22:44:11 -0700

    Trying to fix server

--- server.js
+++ server.js
@@ -7,97 +7,104 @@ var path = require('path');
 var mongoUrl = 'mongodb://irusiabondarenko:35356Dnepr@ds042688.mongolab.com:42688/seekout';
 var testData = [];
 
-  MongoClient.connect(mongoUrl, function(err, db){
-	  if(err) {
-		 console.log('Unable to connect to the mongoDB server. Error:', err);
-	  } else {
-		//HURRAY!! We are connected. :)
-			console.log('Connection established to', mongoUrl);
+  // MongoClient.connect(mongoUrl, function(err, db){
+	  // if(err) {
+		 // console.log('Unable to connect to the mongoDB server. Error:', err);
+	  // } else {
+		// //HURRAY!! We are connected. :)
+			// console.log('Connection established to', mongoUrl);
 
-			var collection = db.collection('lostThings');
+			// var collection = db.collection('lostThings');
 			
 			
-			var app = express();
+			// var app = express();
 
-			app.use(express.static(__dirname));
-			app.use(bodyParser.json());
+			// app.use(express.static(__dirname));
+			// app.use(bodyParser.json());
 			
 			
 			
-			app.get('/', function (req, res) {
-			  res.sendFile('map.html', {root:__dirname});
-			});
+			// app.get('/', function (req, res) {
+			  // res.sendFile('map.html', {root:__dirname});
+			// });
 
-			app.get('/getdata', function(req, res){
-				db.open(function(err,db) {
-					db.collection('lostThings', function(err,collection){
-						collection.find().toArray(function(err, items){
-							res.send(items);
-						})
-					})
-				})
+			// app.get('/getdata', function(req, res){
+				// db.open(function(err,db) {
+					// db.collection('lostThings', function(err,collection){
+						// collection.find().toArray(function(err, items){
+							// res.send(items);
+						// })
+					// })
+				// })
 				
-			});
+			// });
 			
 
-			app.post('/add', function(req, res){
-				var realData = [];
-				collection.insert(req.body, function(err, result){
-					if(err) {
-						console.log('Error inserting data into db');
-					}
-				});
-				realData.push(req.body);
-				res.send(realData);
+			// app.post('/add', function(req, res){
+				// var realData = [];
+				// collection.insert(req.body, function(err, result){
+					// if(err) {
+						// console.log('Error inserting data into db');
+					// }
+				// });
+				// realData.push(req.body);
+				// res.send(realData);
 				
-			});
+			// });
 
-			app.post('/delete', function(req, res){
+			// app.post('/delete', function(req, res){
 			
-				var removeThing = req.body.unique_id;
-			    collection.remove({'unique_id': removeThing})
+				// var removeThing = req.body.unique_id;
+			    // collection.remove({'unique_id': removeThing})
 				
-			});
+			// });
 			
-			app.post('/mail', function (req, res){
+			// app.post('/mail', function (req, res){
 				
-				var mailOpts, smtpTrans;
+				// var mailOpts, smtpTrans;
 				
-				smtpTrans = nodemailer.createTransport('SMTP', {
-				  service: 'Gmail',
-				  auth: {
-					  user: "irusiabondarenko@gmail.com",
-					  pass: "vfylfhbyf23" 
-				  }
-			  });
+				// smtpTrans = nodemailer.createTransport('SMTP', {
+				  // service: 'Gmail',
+				  // auth: {
+					  // user: "irusiabondarenko@gmail.com",
+					  // pass: "vfylfhbyf23" 
+				  // }
+			  // });
 			  
-			  mailOpts = {
-				  to: 'irusiabondarenko@gmail.com',
-				  text: req.body.message,
-				  subject: "Feedback from Seek-out app!"
-			  };
+			  // mailOpts = {
+				  // to: 'irusiabondarenko@gmail.com',
+				  // text: req.body.message,
+				  // subject: "Feedback from Seek-out app!"
+			  // };
 			  
 			  
-			  smtpTrans.sendMail(mailOpts, function (error, response) {
+			  // smtpTrans.sendMail(mailOpts, function (error, response) {
 				  
-				   if (error) {
-						console.log(error);
-					} else {
-						console.log('Message sent');
-					}
-				});
-			  res.send("success")
+				   // if (error) {
+						// console.log(error);
+					// } else {
+						// console.log('Message sent');
+					// }
+				// });
+			  // res.send("success")
 				
-			})
+			// })
 			
 
-			var port = process.env.PORT || 8080;
-			var server = app.listen(port, function(){
-			  var host = server.address().address;
-			  var port = server.address().port;
+			// var port = process.env.PORT || 8080;
+			// var server = app.listen(port, function(){
+			  // var host = server.address().address;
+			  // var port = server.address().port;
 
-			  console.log('Example app listening at http://%s:%s', host, port);
-			});
+			  // console.log('Example app listening at http://%s:%s', host, port);
+			// });
 
-		}
-});
+		// }
+// });
+
+var http = require('http')
+var port = process.env.PORT || 8080;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
