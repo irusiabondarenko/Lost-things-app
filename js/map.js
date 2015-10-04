465137e17feaf2ce3806e8ea71e034720b24cc6f
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-10-04 14:10:48 -0700

    Initial commit

---
+++ js/map.js
@@ -0,0 +1,140 @@
+
+var map, map_var, latLng;
+var myLocation;
+var mapListener;
+var latLng;
+var latNew, lngNew;
+function initMap(coord) {
+	var style = [
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      { "color": "#ffffff" }
+    ]
+  },{
+    "featureType": "water",
+    "stylers": [
+      { "color": "#40a5ff" },
+      { "hue": "#00a1ff" }
+    ]
+  },{
+  }
+]
+ var myOptions = {
+        zoom: 12,
+        center: coord,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: style
+    };
+  map = new google.maps.Map(document.getElementById("map"),
+        myOptions);
+ 
+  map_var = map;
+  return map;
+}
+
+function setMark() {
+	
+    var listener1 =  map.addListener('click', function(e) {
+		 openDialog();
+		 latLng = e.latLng;
+		 latNew = latLng.H;
+	  lngNew = latLng.L;
+		 google.maps.event.removeListener(listener1);
+	});
+	
+	
+		
+}  
+	
+
+function visualizeLostThings(lostThings, map){
+	if(lostThings) {
+		lostThings.forEach(function(item){
+			var marker = new google.maps.Marker({
+				position: item.coord,
+				map: map,
+				icon: image,
+				title: item.title,
+				animation: google.maps.Animation.DROP,
+				markerId: item.unique_id 
+				
+			});
+			var infowindow = new google.maps.InfoWindow({
+				content: getLostThingContent(item),
+				mixWidth: 350,
+			
+			});
+			
+			marker.addListener('click', function(){
+				infowindow.open(map, marker);
+				curMarker = this;
+			});
+			
+			google.maps.event.addListener(infowindow, 'domready', function() {	
+				var iwOuter = $('.gm-style-iw');
+				var iwBackground = iwOuter.prev();
+
+				iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+				iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+				iwOuter.parent().parent().css({left: '115px'});
+				iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+				iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+				iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+				var iwCloseBtn = iwOuter.next();
+				iwCloseBtn.css({opacity: '1', right: '38px', top: '3px', border: '7px solid #48b5e9', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});		
+			});
+					
+		});
+	}
+}
+
+
+			
+function getLocation() {
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(showLocation);
+	}
+}
+var currentPosition;
+function showLocation(position) {
+	currentPosition = position;
+	var map = initMap({lat: position.coords.latitude, lng: position.coords.longitude});
+	
+	$.ajax({
+		url: window.location.origin + '/getdata',
+		success: function(data) {
+			var testData = data;
+			
+				visualizeLostThings(testData, map);
+			
+		}
+	});
+} 
+
+
+$(function(){
+	getLocation();	
+});
+
+var LostThing = function() {
+	this.coord = {
+		lng:'',
+		lat:''
+	};
+	this.title = '';
+	this.description = '';
+	this.contact = '';
+	this.photoURL = '';
+	this.unique_id = guid();
+}
+	
+$(".nav-anchor").hover(function(){
+    $(this).css("color", "white");
+    }, function(){
+    $(this).css("color", "");
+});
+
+	
+	
---
+++ server.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var mongodb = require('mongodb');
+var nodemailer = require('nodemailer'); 
+var MongoClient = mongodb.MongoClient;
+var path = require('path');
+var mongoUrl = 'mongodb://irusiabondarenko:35356Dnepr@ds042688.mongolab.com:42688/seekout';
+var testData = [];
+
+var myDB;
+  MongoClient.connect(mongoUrl, function(err, db){
+	  if(err) {
+		 console.log('Unable to connect to the mongoDB server. Error:', err);
+	  } else {
+		//HURRAY!! We are connected. :)
+			console.log('Connection established to', mongoUrl);
+      
+			var collection = db.collection('lostThings');
+			myDB = db;
+	  }
+	})	
+			
+			var app = express();
+
+			app.use(express.static(__dirname));
+			app.use(bodyParser.json());
+			
+			
+			
+			app.get('/', function (req, res) {
+			  res.sendFile('map.html', {root:__dirname});
+			});
+            setTimeout(function(){ 
+			app.get('/getdata', function(req, res){
+				myDB.open(function(err,myDB) {
+					myDB.collection('lostThings', function(err,collection){
+						collection.find().toArray(function(err, items){
+							res.send(items);
+						})
+					})
+				})
+				
+			});
+			}, 2000);
+			
+ setTimeout(function(){ 
+			app.post('/add', function(req, res){
+				var realData = [];
+				collection.insert(req.body, function(err, result){
+					if(err) {
+						console.log('Error inserting data into db');
+					}
+				});
+				realData.push(req.body);
+				res.send(realData);
+				
+			});
+ }, 2000)
+			app.post('/delete', function(req, res){
+			
+				var removeThing = req.body.unique_id;
+			    collection.remove({'unique_id': removeThing})
+				
+			});
+			
+			app.post('/mail', function (req, res){
+				
+				var mailOpts, smtpTrans;
+				
+				smtpTrans = nodemailer.createTransport('SMTP', {
+				  service: 'Gmail',
+				  auth: {
+					  user: "irusiabondarenko@gmail.com",
+					  pass: "vfylfhbyf23" 
+				  }
+			  });
+			  
+			  mailOpts = {
+				  to: 'irusiabondarenko@gmail.com',
+				  text: req.body.message,
+				  subject: "Feedback from Seek-out app!"
+			  };
+			  
+			  
+			  smtpTrans.sendMail(mailOpts, function (error, response) {
+				  
+				   if (error) {
+						console.log(error);
+					} else {
+						console.log('Message sent');
+					}
+				});
+			  res.send("success")
+				
+			})
+			
+
+			var port = process.env.PORT || 8080;
+			var server = app.listen(port, function(){
+			  var host = server.address().address;
+			  var port = server.address().port;
+
+			  console.log('Example app listening at http://%s:%s', host, port);
+			});
+
+		
