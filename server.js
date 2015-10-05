097339b9b9c7fe7b50e7f1070459be47b8c120c2
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-10-04 19:43:48 -0700

    Added log to getdata

--- server.js
+++ server.js
@@ -9,98 +9,99 @@ var testData = [];
 
 var myDB;
 
-  MongoClient.connect(mongoUrl, function(err, db){
-	  if(err) {
-		 console.log('Unable to connect to the mongoDB server. Error:', err);
-	  } else {
-		//HURRAY!! We are connected. :)
-			console.log('Connection established to', mongoUrl);
-      
-			var collection = db.collection('lostThings');
-			myDB = db;
-	  }
-	})	
-			
-			var app = express();
+MongoClient.connect(mongoUrl, function (err, db) {
+    if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+        //HURRAY!! We are connected. :)
+        console.log('Connection established to', mongoUrl);
 
-			app.use(express.static(__dirname));
-			app.use(bodyParser.json());
+        var collection = db.collection('lostThings');
+        myDB = db;
+    }
+});
 			
-			
-			
-			app.get('/', function (req, res) {
-			  res.sendFile('map.html', {root:__dirname});
-			});
-			app.get('/getdata', function(req, res){
-				if(myDB) {
-					myDB.open(function(err,myDB) {
-					myDB.collection('lostThings', function(err,collection){
-							collection.find().toArray(function(err, items){
-								res.send(items);
-							})
-						})
-					})
-				} else {
-				    res.send('Wait');
-				}
-				
-			});
+var app = express();
 
-			app.post('/add', function(req, res){
-				var realData = [];
-				collection.insert(req.body, function(err, result){
-					if(err) {
-						console.log('Error inserting data into db');
-					}
-				});
-				realData.push(req.body);
-				res.send(realData);
-				
-			});
-
-			app.post('/delete', function(req, res){
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
 			
-				var removeThing = req.body.unique_id;
-			    collection.remove({'unique_id': removeThing})
-				
-			});
 			
-			app.post('/mail', function (req, res){
-				
-				var mailOpts, smtpTrans;
-				
-				smtpTrans = nodemailer.createTransport('SMTP', {
-				  service: 'Gmail',
-				  auth: {
-					  user: "irusiabondarenko@gmail.com",
-					  pass: "vfylfhbyf23" 
-				  }
-			  });
-			  
-			  mailOpts = {
-				  to: 'irusiabondarenko@gmail.com',
-				  text: req.body.message,
-				  subject: "Feedback from Seek-out app!"
-			  };
-			  
-			  
-			  smtpTrans.sendMail(mailOpts, function (error, response) {
-				  
-				   if (error) {
-						console.log(error);
-					} else {
-						console.log('Message sent');
-					}
-				});
-			  res.send("success")
-				
+			
+app.get('/', function (req, res) {
+	res.sendFile('map.html', {root:__dirname});
+});
+app.get('/getdata', function (req, res) {
+    console.log('Get data request arrived');
+	if(myDB) {
+		myDB.open(function(err,myDB) {
+		myDB.collection('lostThings', function(err,collection){
+				collection.find().toArray(function(err, items){
+					res.send(items);
+				})
 			})
+		})
+	} else {
+		res.send('Wait');
+	}
+				
+});
+
+app.post('/add', function(req, res){
+	var realData = [];
+	collection.insert(req.body, function(err, result){
+		if(err) {
+			console.log('Error inserting data into db');
+		}
+	});
+	realData.push(req.body);
+	res.send(realData);
+				
+});
+
+app.post('/delete', function(req, res){
+			
+	var removeThing = req.body.unique_id;
+	collection.remove({'unique_id': removeThing})
+				
+});
+			
+app.post('/mail', function (req, res){
+				
+	var mailOpts, smtpTrans;
+				
+	smtpTrans = nodemailer.createTransport('SMTP', {
+		service: 'Gmail',
+		auth: {
+			user: "irusiabondarenko@gmail.com",
+			pass: "vfylfhbyf23" 
+		}
+	});
+			  
+	mailOpts = {
+		to: 'irusiabondarenko@gmail.com',
+		text: req.body.message,
+		subject: "Feedback from Seek-out app!"
+	};
+			  
+			  
+	smtpTrans.sendMail(mailOpts, function (error, response) {
+				  
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Message sent');
+		}
+	});
+	res.send("success")
+				
+})
 			
 
-			var port = process.env.PORT || 8080;
-			var server = app.listen(port, function(){
-			  var host = server.address().address;
-			  var port = server.address().port;
+var port = process.env.PORT || 8080;
+var server = app.listen(port, function(){
+	var host = server.address().address;
+	var port = server.address().port;
 
-			  console.log('Example app listening at http://%s:%s', host, port);
-			});
+	console.log('Example app listening at http://%s:%s', host, port);
+});
